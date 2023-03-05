1da010f61defba362105e5b905abad1a4762c64a
Yumini <yuminc03@gmail.com>
2023-03-05 14:24:53 +0900

    Initial Commit

---
+++ MyUniverse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyUniverse
+//
+//  Created by Yumin Chu on 2023/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
