a3cb3dcb5d8b72b2377c7ab3a6b6713110d3652e
Yumin Chu <yuminc03@gmail.com>
2023-10-02 11:09:20 +0900

    Refactor: HomeCollectionViewCell flex, pinlayout 적용

--- MyUniverse/Controller/Home/Views/CollectionView/HomeCollectionViewCell.swift
+++ MyUniverse/Controller/Home/Views/CollectionView/HomeCollectionViewCell.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
-import SnapKit
+import FlexLayout
+import PinLayout
 
 final class HomeCollectionViewCell: UICollectionViewCell {
-  
   private let titleLabel: UILabel = {
     let v = UILabel()
     v.textColor = myUniColor(.purple_B080FF)
@@ -24,6 +24,17 @@ final class HomeCollectionViewCell: UICollectionViewCell {
     setupConstraints()
   }
   
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    contentView.flex.layout(mode: .adjustHeight)
+  }
+  
+  override func sizeThatFits(_ size: CGSize) -> CGSize {
+    contentView.flex.width(size.width)
+    contentView.flex.layout(mode: .adjustHeight)
+    return contentView.frame.size
+  }
+  
   required init?(coder: NSCoder) {
     fatalError("Do not use Storyboard.")
   }
@@ -34,10 +45,8 @@ final class HomeCollectionViewCell: UICollectionViewCell {
   }
   
   private func setupConstraints() {
-    titleLabel.snp.makeConstraints {
-      $0.leading.equalToSuperview().offset(20)
-      $0.trailing.lessThanOrEqualToSuperview().offset(-20)
-      $0.top.equalToSuperview().offset(20)
+    contentView.flex.padding(20, 20).define {
+      $0.addItem(titleLabel)
     }
   }
   
