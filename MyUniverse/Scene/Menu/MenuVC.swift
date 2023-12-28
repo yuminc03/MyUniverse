70b38d70bcfe9056bbd38a6a15a0e14efaac7833
Yumin Chu <yuminc03@gmail.com>
2023-12-28 14:23:13 +0900

    Feat: MenuVC - 오픈 예정 문구 추가

--- MyUniverse/Scene/Menu/MenuVC.swift
+++ MyUniverse/Scene/Menu/MenuVC.swift
@@ -7,21 +7,46 @@
 
 import UIKit
 
-import SnapKit
+import FlexLayout
+import PinLayout
 
 final class MenuVC: MyUniVC {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupUI()
-        setupConstraints()
+  private let containerView: UIView = {
+    let v = UIView()
+    v.backgroundColor = .clear
+    return v
+  }()
+  
+  private let noticeLabel: UILabel = {
+    let v = UILabel()
+    v.text = "오픈 예정입니다🙂"
+    v.textColor = .white
+    v.font = .systemFont(ofSize: 30, weight: .bold)
+    v.textAlignment = .center
+    return v
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupUI()
+    setupConstraints()
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    containerView.pin.all(view.pin.safeArea)
+    containerView.flex.layout(mode: .adjustHeight)
+  }
+  
+  private func setupUI() {
+    setNavigationBarTitle("메뉴")
+    view.addSubview(containerView)
+    navi.navigationBar.prefersLargeTitles = true
+  }
+  
+  private func setupConstraints() {
+    containerView.flex.define {
+      $0.addItem(noticeLabel).marginTop(20)
     }
-    
-    private func setupUI() {
-
-    }
-    
-    private func setupConstraints() {
-        
-    }
+  }
 }
