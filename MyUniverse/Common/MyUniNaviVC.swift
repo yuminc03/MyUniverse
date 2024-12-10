0549d4dad1685475c9f6270f30af7cad89553f26
Yumin Chu <yuminc03@gmail.com>
2024-12-10 13:34:49 +0900

    Fix: R.swift를 찾을 수 없다는 오류 수정

--- MyUniverse/Common/MyUniNaviVC.swift
+++ MyUniverse/Common/MyUniNaviVC.swift
@@ -18,8 +18,8 @@ final class MyUniNaviVC: UINavigationController {
   
   private let navigationBarAppearance: UINavigationBarAppearance = {
     let v = UINavigationBarAppearance()
-    v.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(resource: R.color.purple200)!]
-    v.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(resource: R.color.purple200)!]
+    v.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(resource: .purple200)]
+    v.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(resource: .purple200)]
     return v
   }()
   
